e8ccbca6f86b4293a6a59d858902936872a71031
Christopher Maier <chris@operable.io>
2016-08-26 13:52:44 -0400

    Add a JSON tag

---
+++ lib/greenbar/tags/json.ex
@@ -0,0 +1,41 @@
+defmodule Greenbar.Tags.Json do
+  @moduledoc """
+
+  Generates a code block containing the pretty-printed JSON encoding
+  of a variable.
+
+  ## Example
+
+  With `my_json` equal to
+
+      %{"foo" => "bar",
+        "stuff" => %{"hello": "world"}}
+
+  the template
+
+      ~json var=$my_json~
+
+  would render the text
+
+      ```{
+        "foo": "bar",
+        "stuff": {
+          "hello": "world"
+        }
+      }```
+
+  """
+  use Greenbar.Tag
+
+  def name, do: "json"
+
+  def render(attrs, scope) do
+    case get_attr(attrs, "var") do
+      nil ->
+        {:error, "var attribute not set"}
+      var ->
+        {:halt, %{name: :fixed_width, text: "#{Poison.encode!(var, pretty: true)}"}, scope}
+    end
+  end
+
+end
