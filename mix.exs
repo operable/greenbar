d835115f29185c528f454d93f0405d78a7f2315d
Kevin Smith <kevin@operable.io>
2016-11-22 14:21:19 -0500

    Locking deps for 0.16.2 release

--- mix.exs
+++ mix.exs
@@ -23,8 +23,8 @@ defmodule Greenbar.Mixfile do
   defp deps do
     [
       # Direct dependencies
-      {:piper, github: "operable/piper", branch: "v0.16.0"},
-      {:greenbar_markdown, github: "operable/greenbar_markdown", branch: "v0.16.0"},
+      {:piper, github: "operable/piper", tag: "0.16.2"},
+      {:greenbar_markdown, github: "operable/greenbar_markdown", tag: "0.16.2"},
 
       # Test and Development
       {:mix_test_watch, "~> 0.2", only: [:dev, :test]},
